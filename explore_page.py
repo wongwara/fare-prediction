5eb6c20014d782710155bf6d9cd32c48d2fd82a9
wongwara.w <107904836+wongwara@users.noreply.github.com>
2023-11-06 16:08:24 +1100

    Update explore_page.py

--- explore_page.py
+++ explore_page.py
@@ -12,3 +12,18 @@ def load_data():
 
 df = load_data()
 
+def show_explore_page():
+    st.title("💰 Fare Prediction")
+  
+    st.write(
+        """ 
+        The task is to build a data product that will help users in the USA to better estimate their local travel airfare. Users will be able to provide details of their trip and the app will predict the expected flight fare.
+        """
+    ) 
+    st.write(
+        """
+             Therefore, the objective of this project would be to develop a machine learning model that accepts airport name and flightdate return the predict total fare.
+             """
+            )
+
+
