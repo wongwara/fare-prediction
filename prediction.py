17a9bf1ea999f6aebb7de6a9904b2657f0ca1bbe
wongwara.w <107904836+wongwara@users.noreply.github.com>
2023-11-06 18:25:57 +1100

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -4,6 +4,9 @@ import warnings
 warnings.filterwarnings("ignore")
 df = pd.read_csv("https://raw.githubusercontent.com/wongwara/flight-streamlit-at3/main/data/sample_itineraries.csv?token=GHSAT0AAAAAACETSXPAZBNEACHN4ZUWZIW4ZKISTOA")
 
+#drop useless columns
+df = df.drop(columns=['Departure', 'Arrival', 'segmentsDistance', 'segmentsDepartureTimeRaw', 'segmentsArrivalTimeRaw', 'segmentsDurationInSeconds', 'segmentsDistance', 'travelDuration', 'SumsegmentsDurationInSeconds'])
+
 #transform date column: searchDate
 df['searchDate'] = pd.to_datetime(df['searchDate'])
 df['searchDate_day'] = df['searchDate'].dt.day
@@ -18,8 +21,68 @@ df['flightDate_year'] = df['flightDate'].dt.year
 
 #drop date cols
 df = df.drop(columns=['searchDate', 'flightDate'])
+
 df['DepartTime'] = df['segmentsDepartureTimeEpochSeconds'].apply(lambda x: x[0])
+df['DepartTime'] = pd.to_datetime(df['DepartTime'], unit='s')
+
+# Extract and create new columns for hours, minutes, and seconds
+df['DepartTime_hour'] = df['DepartTime'].dt.hour
+df['DepartTime_minute'] = df['DepartTime'].dt.minute
+df['DepartTime_second'] = df['DepartTime'].dt.second
 
 df = df[['totalTravelDistance', 'isNonStop', 'isBasicEconomy', 'startingAirport', 'destinationAirport', 'segmentsCabinCode','flightDate_day', 'flightDate_month', 'flightDate_year',
                          'DepartTime_hour', 'DepartTime_minute', 'DepartTime_second','totalFare']]
+
+cols = df_cleaned.columns.to_list()
+num_cols = df_cleaned.select_dtypes(np.number).columns.to_list()
+cat_cols = list(set(cols) - set(num_cols))
+df['totalTravelDistance']= df['totalTravelDistance'].fillna(df['totalTravelDistance'].mean())
+
+## Lable Encoding
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+df[cat_cols] = df[cat_cols].apply(le.fit_transform)
+#scale numeric column
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+
+df[num_cols] = scaler.fit_transform(df[num_cols])
+
+def pop_target(df, target_col):
+    df_copy = df.copy()
+    target = df_copy.pop(target_col)
+
+    return df_copy, target
+
+features, target = pop_target(df, 'totalFare')
+
+def split_sets_random(features, target, test_ratio=0.2):
+    from sklearn.model_selection import train_test_split
+    val_ratio = test_ratio / (1 - test_ratio)
+    X_data, X_test, y_data, y_test = train_test_split(features, target, test_size=test_ratio, random_state=8)
+    X_train, X_val, y_train, y_val = train_test_split(X_data, y_data, test_size=val_ratio, random_state=8)
+    return X_train, y_train, X_val, y_val, X_test, y_test
+  
+X_train, y_train, X_val, y_val, X_test, y_test = split_sets_random(features, target, test_ratio=0.2)
+
+from sklearn.linear_model import LinearRegression
+
+lr = LinearRegression()
+lr.fit(X_train, y_train)
+
+# Model evaluation for training set
+y_train_preds_lr = lr.predict(X_train)
+y_test_preds_lr = lr.predict(X_test)
+
+import pickle
+data = {"model": linear}
+with open('saved_steps.pkl', 'wb') as file:
+    pickle.dump(data, file)
+  
+def load_model():
+    with open('saved_steps.pkl', 'rb') as file:
+        data = pickle.load(file)
+    return data
+
+regressor_loaded = data["model"]
 
