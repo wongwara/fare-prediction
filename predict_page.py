d02a5f04ac07ca537077ec1b3fe3c192d0663de1
wongwara.w <107904836+wongwara@users.noreply.github.com>
2023-11-07 00:33:45 +1100

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import re
 from datetime import datetime, time
+from prediction import load_model, load_knn_model
 import tensorflow as tf
 import numpy as np
 import joblib
@@ -13,6 +14,9 @@ models_dir = os.path.abspath('models')
 # Load the KNN model with the full path
 knn_model_path = os.path.join(models_dir, 'knn_model.joblib')
 knn_model = joblib.load(knn_model_path)
+
+regressor_loaded = load_model()
+knn_regressor_loaded = load_knn_model()
 
 # Load the model using a relative path
 # knn_model_path = "./models/knn.joblib"
@@ -155,7 +159,7 @@ def show_predict_page():
         X = X.drop(columns=['flightDate','departureTime'])
         X = X[['totalTravelDistance', 'isNonStop', 'isBasicEconomy', 'startingAirport', 'destinationAirport', 'segmentsCabinCode','flightDate_day', 'flightDate_month', 'flightDate_year',
                          'DepartTime_hour', 'DepartTime_minute', 'DepartTime_second']]
-        total_fare = knn_model.predict(X)
+        total_fare = knn_regressor_loaded.predict(X)
         total_fare = np.round(total_fare, 2)  # Round the value to two digits
         total_fare_str = str(total_fare[0])  # Convert to string
         st.write(f"The total fare for your trip {total_fare_str}$")
