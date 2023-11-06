a7208f1bdd372e437381973e6abd2fc865120526
Thirada Tiamklang <114060344+thirada2799@users.noreply.github.com>
2023-11-06 18:25:32 +1100

    Update predict_page.py ignore

--- predict_page.py
+++ predict_page.py
@@ -8,7 +8,7 @@ import tensorflow as tf
 # data = load_model()
 # regressor_loaded = data["model"]
 
-loaded_model = tf.keras.models.load_model("../models/tfdf_model")
+# loaded_model = tf.keras.models.load_model("../models/tfdf_model")
 
 def show_predict_page():
     st.title(" ✈️ Fare Prediction")
@@ -147,6 +147,6 @@ def show_predict_page():
     st.write("Destination Airport:", destination_airport)
     st.write("Departure Time:", departure_time)
 
-    predictions = loaded_model.predict(X)
-    st.write("Predicted Total Fare:", predictions[0])
+    # predictions = loaded_model.predict(X)
+    # st.write("Predicted Total Fare:", predictions[0])
 
