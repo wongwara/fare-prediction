2b1ce914eac0c5e0464ac557f9a673afacceca8a
wongwara.w <107904836+wongwara@users.noreply.github.com>
2023-11-06 14:55:21 +1100

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -1,1 +1,8 @@
+import streamlit as st
+import pandas as pd
+import re
 
+def show_predict_page():
+    st.title("🤖 Salary Prediction")
+    st.write(""" This project is for the job-seeker and students to searching the expected salary from related information, such as location or job subclassification""")
+    st.subheader("We need some information to predict the salary")
