8052aa92de51218a509f94dee6249b8efa8dbc4f
wongwara.w <107904836+wongwara@users.noreply.github.com>
2023-11-06 14:27:03 +1100

    Update explore_page.py

--- explore_page.py
+++ explore_page.py
@@ -1,1 +1,15 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objs as go
+import seaborn as sns
+import numpy as np
+
+def load_data():
+    df = pd.read_csv("https://raw.githubusercontent.com/wongwara/predict_next_month/main/df_monthly.csv")
+    
+    return df
+
+df = load_data()
 
